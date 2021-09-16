1e19dc34dff83f869a0621e735864981d8fd97f3
parth782 <parth11gupta@gmail.com>
2021-09-16 17:44:04 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("hello");
+let screenvalue="";
+let screen=document.getElementById('screen');
+let buttons=document.querySelectorAll('button');
+for( item of buttons){
+    item.addEventListener("click",(e)=>{
+        buttontext=e.target.innerText;
+        console.log(buttontext);
+        if(buttontext=='x'){
+            buttontext='*';
+            screenvalue+=buttontext
+            screen.value=screenvalue;
+        }
+        else if(buttontext=='='){
+           try{
+               eval(screenvalue)
+               
+           }
+           catch(err){
+               
+            screen.value="";
+               screenvalue="";
+               console.log("inside catch");
+               
+           }
+           screen.value=eval(screenvalue);
+            
+        }
+        else if(buttontext=='C'){
+            screen.value="";
+            screenvalue="";
+        }
+        else{
+            screenvalue+=buttontext
+            screen.value=screenvalue;
+        }
+    })
+}
